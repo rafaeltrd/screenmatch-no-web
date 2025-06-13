8569360cf7fde630fcebcae5e1b4bf08353e05c7
Rafael Santos Trindade <rafael.st@sempreceub.com>
2025-06-13 17:12:48 -0300

    Criação e implementação SeasonInfo

--- src/main/java/br/com/alura/screenmatch/ScreenMatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenMatchApplication.java
@@ -1,12 +1,16 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.model.EpisodeInfo;
+import br.com.alura.screenmatch.model.SeasonInfo;
 import br.com.alura.screenmatch.model.SeriesInfo;
 import br.com.alura.screenmatch.service.APIConsumption;
 import br.com.alura.screenmatch.service.DataConverter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenMatchApplication implements CommandLineRunner {
@@ -16,7 +20,7 @@ public class ScreenMatchApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args){
 		var apiConsumption = new APIConsumption();
 		var json = apiConsumption.getData("http://www.omdbapi.com/?apikey=20731567&t=Breaking+Bad");
 		System.out.println(json);
@@ -32,5 +36,17 @@ public class ScreenMatchApplication implements CommandLineRunner {
 		json = apiConsumption.getData("http://www.omdbapi.com/?apikey=20731567&t=Breaking+Bad&Season=1&Episode=4");
 		EpisodeInfo episodeInfo = converter.getData(json, EpisodeInfo.class);
 		System.out.println(episodeInfo);
+
+		System.out.println("\n-------------\n");
+
+		List<SeasonInfo> seasonList = new ArrayList<>();
+
+		for(int i = 1; i <= seriesInfo.totalSeasons(); i++){
+			json = apiConsumption.getData("http://www.omdbapi.com/?apikey=20731567&t=Breaking+Bad&Season=" + i);
+			SeasonInfo seasonInfo = converter.getData(json, SeasonInfo.class);
+			seasonList.add(seasonInfo);
+		}
+
+			seasonList.forEach(System.out::println);
 	}
 }
--- src/main/java/br/com/alura/screenmatch/model/SeasonInfo.java
+++ src/main/java/br/com/alura/screenmatch/model/SeasonInfo.java
@@ -3,7 +3,9 @@ package br.com.alura.screenmatch.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record EpisodeInfo(@JsonAlias("Episode") Integer episode,
-                          @JsonAlias("Title") String title, @JsonAlias("Released") String released) {
+public record SeasonInfo(@JsonAlias("Season") String season,
+                         @JsonAlias("Episodes") List<EpisodeInfo> episodes) {
 }
